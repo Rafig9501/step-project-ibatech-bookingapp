f0034bc9f7b53f79e140eeb9ee78967b4beb30ec
Rafig <rafiqmammedzade@gmail.com>
2020-03-16 14:06:37 +0400

    project has been written without junit

---
+++ src/main/java/utilities/Constants_.java
@@ -0,0 +1,15 @@
+package utilities;
+
+public class Constants_ {
+
+    final public static String SIGN_IN = "1";
+    final public static String LOG_IN = "2";
+    final public static String DELETE_USER = "3";
+    final public static String EXIT = "4";
+
+    final public static String SHOW_FLIGHTS = "1";
+    final public static String MAKE_BOOKING = "2";
+    final public static String SHOW_MY_BOOKINGS = "3";
+    final public static String CANCEL_BOOKING = "4";
+    final public static String LOG_OUT = "5";
+}
